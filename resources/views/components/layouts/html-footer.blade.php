211d040a6a0f8c634f236c8d9c360649eff04721
Darnell Collins <collinsdarnell44@gmail.com>
2025-12-03 18:24:27 -0500

    app name in footer

--- resources/views/components/layouts/html-footer.blade.php
+++ resources/views/components/layouts/html-footer.blade.php
@@ -1,1 +1,10 @@
+<footer class="bg-white shadow mt-8">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p class="text-center text-sm text-gray-500">
+            © {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+        </p>
+    </div>
+</footer>
+
+</body>
 </html>
